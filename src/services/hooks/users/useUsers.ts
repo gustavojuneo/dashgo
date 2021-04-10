35e512c93227a6025f42876a6b9aa7cd9367dd06
Gustavo Juneo <gustavojuneo@outlook.com>
2021-04-09 21:34:48 -0300

    mirage users pagination

--- src/services/hooks/users/useUsers.ts
+++ src/services/hooks/users/useUsers.ts
@@ -2,35 +2,33 @@ import { useQuery } from 'react-query'
 import { api } from '../../api'
 
 type User = {
-  id: number
+  id: string
   name: string
   email: string
   createdAt: string
 }
 
-export function useUsers() {
-  return useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('users')
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get('users')
 
-      const users = data.users.map((user: User) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-          })
-        }
+  const users = data.users.map((user: User) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
       })
-
-      return users
-    },
-    {
-      staleTime: 1000 * 5 // 5 seconds
     }
-  )
+  })
+
+  return users
+}
+
+export function useUsers() {
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5 // 5 seconds
+  })
 }
