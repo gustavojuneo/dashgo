4904d5d2a538b8a38a2a8520056caaccafd9238d
Gustavo Juneo <gustavojuneo@outlook.com>
2021-04-09 21:19:03 -0300

    add axios and create useUsers hook

---
+++ src/services/hooks/users/useUsers.ts
@@ -0,0 +1,36 @@
+import { useQuery } from 'react-query'
+import { api } from '../../api'
+
+type User = {
+  id: number
+  name: string
+  email: string
+  createdAt: string
+}
+
+export function useUsers() {
+  return useQuery(
+    'users',
+    async () => {
+      const { data } = await api.get('users')
+
+      const users = data.users.map((user: User) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+          })
+        }
+      })
+
+      return users
+    },
+    {
+      staleTime: 1000 * 5 // 5 seconds
+    }
+  )
+}
